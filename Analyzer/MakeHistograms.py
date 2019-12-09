826d07fa8b915a6f1b6aa451a65d1042d23a5927
Abu Ubaidah <abu.ubaidah@cern.ch>
2019-12-09 12:17:05 +0100

    Make histograms for multiple root files

--- Analyzer/MakeHistograms.py
+++ Analyzer/MakeHistograms.py
@@ -4,6 +4,7 @@ import glob
 import ROOT
 from collections import OrderedDict 
 import VariableList
+import SampleList
 ROOT.gROOT.SetBatch()
 #
 # Only on lxplus7
@@ -14,39 +15,54 @@ treeName="Events"
 
 ROOT.ROOT.EnableImplicitMT(8)
 
-def main():
-  # file to be read from
-  INFILE=EOSUSER+"/eos/user/n/nbinnorj/CRABOUTPUT_JetPUId_DiMuonSkim_v0/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/JetPUId_MC16NanoAODv5_ext1-v1_DiMuonSkim_v0/191123_043755/0000/tree_1.root"
+def makeHistogram(sample_name):
 
-# how to read a file in RDataFrame
-  df = ROOT.ROOT.RDataFrame(treeName, INFILE)
+  print("Processing %s" %sample_name)
+
+  for files in SampleList.("%s" %sample_name).files:
+    FileList = glob.glob(files)
+  
+  # Creating std::vector as filelist holder to be plugged into RDataFrame
+  vec = ROOT.vector('string')()
+
+  for f in FileList:
+    vec.push_back(f)
+
+  # how to read a file in RDataFrame
+  df = ROOT.ROOT.RDataFrame(treeName, vec)
   numOfEvents = df.Count()
   print("Number of all events: %s " %numOfEvents.GetValue())
   
-# branches to be read
-    # br = var
-  # cut1 = "mu0_mass < 60 && mu0_mass > 30"
-  # df_filtered = df.Filter(cut1)
-  # numOfEvents_filtered = df_filtered.Count()
-  # print("Number of filtered events: %s" %numOfEvents_filtered.GetValue())
-  
-# create histogram dictionary
+  # Create histogram dictionary
   Histograms = {}
 
+  #making histogram
   for varName in VariableList.Variables:
     var = VariableList.Variables[varName]
     print "Creating %s histogram." %var.varNameInTree
     Histograms[var.varNameInTree] = df.Histo1D((var.varNameInTree, var.varNameInTree, var.nbins, var.xmin, var.xmax), var.varNameInTree)
     # h_filtered = df_filtered.Histo1D((var.varNameInTree, var.varNameInTree, var.nbins, var.xmin, var.xmax), var.varNameInTree)
 
-# Open a new ROOT file to store TH1
-  f = ROOT.TFile('test.root', 'RECREATE')
+  # Open a new ROOT file to store TH1
+  f = ROOT.TFile(sample_name+'.root', 'RECREATE')
 
-# Loop over the Histograms dictionary and store TH1 in ROOT file
+  # Loop over the Histograms dictionary and store TH1 in ROOT file
   for histName in Histograms:
     Histograms[histName].Write()
-  
+    
   f.Close()
+
+def main():
+
+  makeHistogram("MC16_DY_MG")
+  makeHistogram("MC16_DY_AMCNLO")
+  makeHistogram("Data16B_DoubleMuon")
+  makeHistogram("Data16C_DoubleMuon")
+  makeHistogram("Data16D_DoubleMuon")
+  makeHistogram("Data16E_DoubleMuon")
+  makeHistogram("Data16F_DoubleMuon")
+  makeHistogram("Data16G_DoubleMuon")
+  makeHistogram("Data16H_DoubleMuon")
 
 if __name__== "__main__":
   main()
