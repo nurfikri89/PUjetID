0bd41d687c81346bcd7a72f9ca0854901c51bf28
Abu Ubaidah <abu.ubaidah@cern.ch>
2019-12-10 04:06:23 +0100

    Convert SampleList into dictionary

--- Analyzer/MakeHistograms.py
+++ Analyzer/MakeHistograms.py
@@ -15,54 +15,44 @@ treeName="Events"
 
 ROOT.ROOT.EnableImplicitMT(8)
 
-def makeHistogram(sample_name):
-
-  print("Processing %s" %sample_name)
-
-  for files in SampleList.("%s" %sample_name).files:
-    FileList = glob.glob(files)
-  
-  # Creating std::vector as filelist holder to be plugged into RDataFrame
-  vec = ROOT.vector('string')()
-
-  for f in FileList:
-    vec.push_back(f)
-
-  # how to read a file in RDataFrame
-  df = ROOT.ROOT.RDataFrame(treeName, vec)
-  numOfEvents = df.Count()
-  print("Number of all events: %s " %numOfEvents.GetValue())
-  
-  # Create histogram dictionary
-  Histograms = {}
-
-  #making histogram
-  for varName in VariableList.Variables:
-    var = VariableList.Variables[varName]
-    print "Creating %s histogram." %var.varNameInTree
-    Histograms[var.varNameInTree] = df.Histo1D((var.varNameInTree, var.varNameInTree, var.nbins, var.xmin, var.xmax), var.varNameInTree)
-    # h_filtered = df_filtered.Histo1D((var.varNameInTree, var.varNameInTree, var.nbins, var.xmin, var.xmax), var.varNameInTree)
-
-  # Open a new ROOT file to store TH1
-  f = ROOT.TFile(sample_name+'.root', 'RECREATE')
-
-  # Loop over the Histograms dictionary and store TH1 in ROOT file
-  for histName in Histograms:
-    Histograms[histName].Write()
-    
-  f.Close()
-
 def main():
 
-  makeHistogram("MC16_DY_MG")
-  makeHistogram("MC16_DY_AMCNLO")
-  makeHistogram("Data16B_DoubleMuon")
-  makeHistogram("Data16C_DoubleMuon")
-  makeHistogram("Data16D_DoubleMuon")
-  makeHistogram("Data16E_DoubleMuon")
-  makeHistogram("Data16F_DoubleMuon")
-  makeHistogram("Data16G_DoubleMuon")
-  makeHistogram("Data16H_DoubleMuon")
+  for sample_name in SampleList.Samples:
+    # print("Processing %s" %sample_name)
+    FileList = []
+    for files in SampleList.Samples[sample_name].files:
+      # print("Processing %s" %files)
+      FileList += glob.glob(files)
+      
+    # Creating std::vector as filelist holder to be plugged into RDataFrame
+    vec = ROOT.vector('string')()
+  
+    for f in FileList:
+      vec.push_back(f)
+  
+    # how to read a file in RDataFrame
+    df = ROOT.ROOT.RDataFrame(treeName, vec)
+    numOfEvents = df.Count()
+    print("Number of all events: %s " %numOfEvents.GetValue())
+    
+    # Create histogram dictionary
+    Histograms = {}
+  
+    #making histogram
+    for varName in VariableList.Variables:
+      var = VariableList.Variables[varName]
+      print "Creating %s histogram." %var.varNameInTree
+      Histograms[var.varNameInTree] = df.Histo1D((var.varNameInTree, var.varNameInTree, var.nbins, var.xmin, var.xmax), var.varNameInTree)
+      # h_filtered = df_filtered.Histo1D((var.varNameInTree, var.varNameInTree, var.nbins, var.xmin, var.xmax), var.varNameInTree)
+  
+    # Open a new ROOT file to store TH1
+    f = ROOT.TFile(sample_name+'.root', 'RECREATE')
+  
+    # Loop over the Histograms dictionary and store TH1 in ROOT file
+    for histName in Histograms:
+      Histograms[histName].Write()
+      
+    f.Close()
 
 if __name__== "__main__":
   main()
--- Analyzer/SampleList.py
+++ Analyzer/SampleList.py
@@ -4,23 +4,27 @@ class Sample:
       self.files = files 
 
 version="DiMuonSkim_v0"
+EOSUSER="root://eosuser.cern.ch/"
 EOSDIR="/eos/user/n/nbinnorj/CRABOUTPUT_JetPUId_"+version+"/"
 
-MC16_DY_MG = Sample(
+Samples = {}
+
+Samples["MC16_DY_MG"] = Sample(
   name="MC16_DY_MG",
   files=[
     EOSDIR+"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/JetPUId_MC16NanoAODv5_ext1-v1_"+version+"/*/*/tree_*.root",
     EOSDIR+"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/JetPUId_MC16NanoAODv5_ext2-v1_"+version+"/*/*/tree_*.root"
   ]
 )
-MC16_DY_AMCNLO = Sample(
+
+Samples["MC16_DY_AMCNLO"] = Sample(
   name="MC16_DY_AMCNLO",
   files=[
     EOSDIR+"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/JetPUId_MC16NanoAODv5_ext2-v1_"+version+"/*/*/tree_*.root",
   ]
 )
 
-Data16B_DoubleMuon = Sample(
+Samples["Data16B_DoubleMuon"] = Sample(
   name="Data16B_DoubleMuon",
   files=[
     EOSDIR+"DoubleMuon/JetPUId_Run2016B_ver1-Data16NanoAODv5_ver1-v1_"+version+"/*/*/tree_*.root",
@@ -28,42 +32,42 @@ Data16B_DoubleMuon = Sample(
   ]
 )
 
-Data16C_DoubleMuon = Sample(
+Samples["Data16C_DoubleMuon"] = Sample(
   name="Data16C_DoubleMuon",
   files=[
     EOSDIR+"DoubleMuon/JetPUId_Run2016C_ver1-Data16NanoAODv5_ver1-v1_"+version+"/*/*/tree_*.root"
   ]
 )
 
-Data16D_DoubleMuon = Sample(
+Samples["Data16D_DoubleMuon"] = Sample(
   name="Data16D_DoubleMuon",
   files=[
     EOSDIR+"DoubleMuon/JetPUId_Run2016D_ver1-Data16NanoAODv5_ver1-v1_"+version+"/*/*/tree_*.root"
   ]
 )
 
-Data16E_DoubleMuon = Sample(
+Samples["Data16E_DoubleMuon"] = Sample(
   name="Data16E_DoubleMuon",
   files=[
     EOSDIR+"DoubleMuon/JetPUId_Run2016E_ver1-Data16NanoAODv5_ver1-v1_"+version+"/*/*/tree_*.root"
   ]
 )
 
-Data16F_DoubleMuon = Sample(
+Samples["Data16F_DoubleMuon"] = Sample(
   name="Data16F_DoubleMuon",
   files=[
     EOSDIR+"DoubleMuon/JetPUId_Run2016F_ver1-Data16NanoAODv5_ver1-v1_"+version+"/*/*/tree_*.root"
   ]
 )
 
-Data16G_DoubleMuon = Sample(
+Samples["Data16G_DoubleMuon"] = Sample(
   name="Data16G_DoubleMuon",
   files=[
     EOSDIR+"DoubleMuon/JetPUId_Run2016G_ver1-Data16NanoAODv5_ver1-v1_"+version+"/*/*/tree_*.root"
   ]
 )
 
-Data16H_DoubleMuon = Sample(
+Samples["Data16H_DoubleMuon"] = Sample(
   name="Data16H_DoubleMuon",
   files=[
     EOSDIR+"DoubleMuon/JetPUId_Run2016H_ver1-Data16NanoAODv5_ver1-v1_"+version+"/*/*/tree_*.root"
