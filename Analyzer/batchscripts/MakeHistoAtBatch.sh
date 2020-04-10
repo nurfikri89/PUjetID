0bbd6334d4f9ee256fe1ca7d0960bf6be2e78fa8
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2020-04-10 20:25:05 +0200

    Update batch script

--- Analyzer/batchscripts/MakeHistoAtBatch.sh
+++ Analyzer/batchscripts/MakeHistoAtBatch.sh
@@ -2,9 +2,7 @@
 
 cd REPLACEME
 source ./sourceRecentROOT.sh
-#
-#
+
+python SkimNtuples.py --sample ${1} --cores ${2}
+
 python MakeHistograms.py --sample ${1} --cores ${2}
-#
-#
-# python SkimNtuples.py --sample ${1} --cores ${2}
