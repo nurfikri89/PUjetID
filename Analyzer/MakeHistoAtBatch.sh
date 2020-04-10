29436f033fc23c52c7e3665b310cc285aa6783de
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2020-04-10 20:21:34 +0200

    Remove copied bash scripts

--- Analyzer/MakeHistoAtBatch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /afs/cern.ch/user/n/nbinnorj/work/AnaPUJetID_cmsjet/CMSSW_10_2_18/src/PUjetID/Analyzer
-source ./sourceRecentROOT.sh
-#
-python SkimNtuples.py --sample ${1} --cores ${2}
-python MakeHistograms.py --sample ${1} --cores ${2}
