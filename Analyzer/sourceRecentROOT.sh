823b1e58cd5737f16cf6cb9dc0be499622ff1e34
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2020-04-12 15:46:32 +0200

    Update README

--- Analyzer/sourceRecentROOT.sh
+++ Analyzer/sourceRecentROOT.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+if [ ! -z $CMSSW_BASE ]; then 
+  echo "Unset CMSSW runtime environment"
+  eval `scram unsetenv -sh`
+fi
+#
+echo "Setup ROOT 6.18.04"
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.18.04/x86_64-centos7-gcc48-opt/bin/thisroot.sh
