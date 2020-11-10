cbca37f9c1403c46cc3d55a698bb305fd775a1fd
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2020-11-10 15:40:37 +0100

    Update to run over JMEnanoV1 UL17

--- Skimmer/RunLocal.sh
+++ Skimmer/RunLocal.sh
@@ -1,25 +1,44 @@
-MAX_EVENTS=50000
 #
+# EOY 2016
 #
-python RunSkimmerLocal.py \
---era="2016" \
---maxEvents=${MAX_EVENTS} \
---outDir="./MC16_DYJetsToLL_NLO" \
---isMC=1 \
---dataStream="MC"
+# MAX_EVENTS=50000
+# python RunSkimmerLocal.py \
+# --era="2016" \
+# --maxEvents=${MAX_EVENTS} \
+# --outDir="./MC16_DYJetsToLL_NLO" \
+# --isMC=1 \
+# --dataStream="MC"
+# #
+# MAX_EVENTS=50000
+# python RunSkimmerLocal.py \
+# --era="2016" \
+# --maxEvents=${MAX_EVENTS} \
+# --outDir="./Data16_DoubleMuon" \
+# --isMC=0 \
+# --dataStream="DoubleMuon"
+# #
+# MAX_EVENTS=50000
+# python RunSkimmerLocal.py \
+# --era="2016" \
+# --maxEvents=${MAX_EVENTS} \
+# --outDir="./Data16_DoubleEG" \
+# --isMC=0 \
+# --dataStream="DoubleEG"
 #
+# UL 2017 JMEnano v1
 #
-python RunSkimmerLocal.py \
---era="2016" \
---maxEvents=${MAX_EVENTS} \
---outDir="./Data16_DoubleMuon" \
---isMC=0 \
---dataStream="DoubleMuon"
+# MAX_EVENTS=20000
+# python RunSkimmerLocal.py \
+# --era="UL2017" \
+# --maxEvents=${MAX_EVENTS} \
+# --outDir="./MCUL17_DYJetsToLL_LO" \
+# --isMC=1 \
+# --dataStream="MC"
 #
-#
-python RunSkimmerLocal.py \
---era="2016" \
---maxEvents=${MAX_EVENTS} \
---outDir="./Data16_DoubleEG" \
---isMC=0 \
---dataStream="DoubleEG"
+# MAX_EVENTS=20000
+# python RunSkimmerLocal.py \
+# --era="UL2017" \
+# --maxEvents=${MAX_EVENTS} \
+# --outDir="./DataUL17_DoubleMuon" \
+# --isMC=0 \
+# --dataStream="DoubleMuon"
